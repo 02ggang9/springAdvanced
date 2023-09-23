fefba5072e8442308f289ae3f9afaa48fa2b1df8
02ggang9 <02ggang9@gmail.com>
2023-09-23 15:45:47 +0900

    섹션2 - 필드 동기화 - 동시성 문제

---
+++ src/main/java/hello/advanced/app/v3/OrderControllerV3.java
@@ -0,0 +1,33 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+
+    private final OrderServiceV3 orderService;
+    private final LogTrace trace;
+
+    @GetMapping("/v3/request")
+    public String request(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져주어야 한다.
+        }
+    }
+}
+
+// 로그 추적기 - 요구사항 분석
+// 모니터링 툴을 도입하면 많은 부분이 해결되지만, 그걸로도 해결이 안되는 부분이 있는 경우에는 로그 추적기 붙여놓으면 좋다!
